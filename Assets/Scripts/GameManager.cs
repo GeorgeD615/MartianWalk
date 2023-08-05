d5c98e652645764e8629fb0dc031b44ac4a9632e
GeorgeD615 <st095277@student.spbu.ru>
2023-08-05 14:18:18 +0300

    Add pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject _healthUI;
     [SerializeField] private GameObject _menuUI;
     [SerializeField] private GameObject _settingsUI;
+    [SerializeField] private GameObject _pauseMenuUI;
 
     [SerializeField] private GameObject _playerPref;
 
@@ -22,6 +23,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera _playerCamera;
 
     [SerializeField] private Transform _leftRobotPosition;
+
+    [SerializeField] private EnemyBehavior[] _enemyBehavior;
+    [SerializeField] private EnemyThrow[] _enemyThrows;
+    [SerializeField] private EnemyRotation[] _enemyRotations;
+    [SerializeField] private EnemyHealth[] _enemyHealths;
 
     private CinemachineVirtualCamera _currentCamera;
 
@@ -40,17 +46,47 @@ public class GameManager : MonoBehaviour
         }
         DontDestroyOnLoad(gameObject);
 
-        if (_checkpointLoaded)
+        MainMenuEnable();
+        SettingsMenuDisable();
+        PlayerControlDisable();
+        PauseMenuDisable();
+    }
+
+    private bool _onPause;
+
+    public void PauseOff()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void PauseOn()
+    {
+        Time.timeScale = 0f;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !_menuUI.activeSelf && !_settingsCamera.enabled)
         {
-            PlayerControlEnable();
-            MainMenuDisable();
-            SettingsMenuDisable();
-        }
-        else
-        {
-            MainMenuEnable();
-            SettingsMenuDisable();
-            PlayerControlDisable();
+            if (_onPause)
+            {
+                _onPause = false;
+                Time.timeScale = 1f;
+                EnableEnemys();
+                PlayerControlEnable();
+                SwitchCamera(_playerCamera);
+                Cursor.visible = false;
+                _pauseMenuUI.SetActive(false);
+                _settingsUI.SetActive(false);
+            }
+            else
+            {
+                _onPause = true;
+                Time.timeScale = 0f;
+                PlayerControlDisable();
+                DisableEnemys();
+                Cursor.visible = true;
+                _pauseMenuUI.SetActive(true);
+            }
         }
     }
 
@@ -88,11 +124,22 @@ public class GameManager : MonoBehaviour
         _menuUI.SetActive(false);
     }
 
+    public void PauseMenuEnable()
+    {
+        _pauseMenuUI.SetActive(true);
+    }
+
+    public void PauseMenuDisable()
+    {
+        _pauseMenuUI.SetActive(false);
+    }
+
     public void SettingsMenuEnable()
     {
         _settingsUI.SetActive(true);
         Cursor.visible = true;
-        SwitchCamera(_settingsCamera);
+        if(!_onPause)
+            SwitchCamera(_settingsCamera);
     }
 
     public void SettingsMenuDisable()
@@ -107,6 +154,9 @@ public class GameManager : MonoBehaviour
 
     private void SwitchCamera(CinemachineVirtualCamera newCamera) 
     {
+        if (newCamera == _currentCamera)
+            return;
+
         newCamera.enabled = true;
         
         if(_currentCamera != null)
@@ -122,5 +172,58 @@ public class GameManager : MonoBehaviour
         PlayerMovement.Instance.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
         RobotMovement.Instanse.transform.position = _leftRobotPosition.position;
         PlayerHealth.Instance.ResetHealth();
+    }
+
+    public void EnableEnemys()
+    {
+        foreach (EnemyHealth obj in _enemyHealths)
+        {
+            obj.enabled = true;
+        }
+        foreach (EnemyRotation obj in _enemyRotations)
+        {
+            obj.enabled = true;
+        }
+        foreach (EnemyThrow obj in _enemyThrows)
+        {
+            obj.enabled = true;
+        }
+        foreach (EnemyBehavior obj in _enemyBehavior)
+        {
+            obj.enabled = true;
+        }
+    }
+
+    public void DisableEnemys()
+    {
+        foreach (EnemyHealth obj in _enemyHealths)
+        {
+            obj.enabled = false;
+        }
+        foreach (EnemyRotation obj in _enemyRotations)
+        {
+            obj.enabled = false;
+        }
+        foreach (EnemyThrow obj in _enemyThrows)
+        {
+            obj.enabled = false;
+        }
+        foreach (EnemyBehavior obj in _enemyBehavior)
+        {
+            obj.enabled = false;
+        }
+    }
+
+    public void BackButtonPressed()
+    {
+        SettingsMenuDisable();
+        if (_onPause)
+        {
+            PauseMenuEnable();
+        }
+        else
+        {
+            MainMenuEnable();
+        }
     }
 }
